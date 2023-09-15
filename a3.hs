8db94c1a4e907c16310d8e8489a17fd6db77b9f8
Shreyas Casturi <Shreyas.Casturi@gtri.gatech.edu>
2023-09-15 09:28:49 -0400

    a3 file added

---
+++ a3.hs
@@ -0,0 +1,22 @@
+-- snoc 5 [1, 2, 3] = [1, 2, 3, 5]
+snoc :: a -> [a] -> [a]
+snoc a [] = [a]
+snoc a (y:ys) = y : (snoc a ys)
+
+-- get last element of list
+getLast :: [a] -> Maybe a 
+getLast [] = Nothing
+getLast [a] = Just a 
+getLast (y:ys) = getLast ys
+
+-- basic add oeprator
+myappend :: [a] -> [a] -> [a]
+myappend [] (y:ys) = (y:ys)
+myappend (y:ys) [] = (y:ys)
+myappend [x] [y] = x:[y]
+myappend (x:xs) (y:ys) = x:(myappend xs (y:ys))
+
+myreverse :: [a] -> [a]
+myreverse [] = []
+myreverse [x] = [x]
+myreverse (y:ys) = myappend (myreverse ys) [y]
